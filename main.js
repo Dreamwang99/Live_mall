6d2225f6eacae1ad75857029f475742ecffa6c19
Dreamwang99 <1097289170@qq.com>
2019-12-20 12:07:58 +0800

    王子升

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import App from './App'
 import api from "./api/index"
-// import vConsole from '@/common/vconsole/dist/vconsole.min.js'
+import vConsole from '@/common/vconsole/dist/vconsole.min.js'
 
 Vue.config.productionTip = false
 
 App.mpType = 'app'
 
-// const vConsoles = new vConsole()
+const vConsoles = new vConsole()
 Vue.prototype.baseUrl ="http://zhibo.a2w0m.cn/api/public/?service="
 Vue.prototype.request = api
 
